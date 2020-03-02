3128d8bc6c8db44f668f8d600e9954053ee1ac42
Robert Schwarz <mail@rschwarz.net>
2020-03-02 12:47:51 +0100

    add failing test (only segmentlist given)

---
+++ test/test_triangulate2.jl
@@ -0,0 +1,26 @@
+module test_triangulate2
+using Triangulate
+
+function test()
+    #     *
+    #     |
+    #  *--*--*
+
+    triin = Triangulate.TriangulateIO(
+        pointlist = Float64[0.0 1.0 2.0 1.0
+                            0.0 0.0 0.0 1.0],
+        segmentlist = Int32[1 2 2
+                            2 3 4],
+        # don't give segmentmarkerlist !
+    )
+
+    (triout, vorout) = triangulate("peDQ", triin)
+
+    return all([
+        size(triout.pointlist, 2) == 4,
+        size(triout.trianglelist, 2) == 2,
+        size(triout.segmentlist, 2) == 3,
+    ])
+end
+
+end
