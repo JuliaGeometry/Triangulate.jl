7c80ce0f72b06a6314093ff2820ba2a4de088f84
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2023-11-13 15:19:11 +0100

    @compat public

--- src/Triangulate.jl
+++ src/Triangulate.jl
@@ -1,18 +1,26 @@
 module Triangulate
 using DocStringExtensions
 using Triangle_jll
+using Compat: @compat
 
 include("ctriangulateio.jl")
 include("triangulateio.jl")
 include("plot.jl")
 
 export triangulate
-export triunsuitable
+export triunsuitable!, triunsuitable
 export TriangulateIO
 export numberofpoints
 export numberofsegments
 export numberoftriangles
 export TriangulateError
-export plot_triangulateio, plot_in_out, isplots, ispyplot
+export plot_triangulateio, plot_in_out
+export isplots, ispyplot
+
+@compat public triangulate, triunsuitable, triunsuitable!
+@compat public numberofpoints, numberofsegments, numberoftriangles
+@compat public TriangulateError
+@compat public plot_triangulateio, plot_in_out
+@compat public isplots, ispyplot
 
 end
